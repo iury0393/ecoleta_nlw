b982bf90e04740dfe9341b750c48ef86e12adfd2
Iury Vasconcelos <iury0393@gmail.com>
2020-06-02 18:38:36 -0300

    Listagem dos items

--- server/src/database/migrations/01_create_items.ts
+++ server/src/database/migrations/01_create_items.ts
@@ -3,7 +3,7 @@ import Knex from 'knex';
 export async function up(knex: Knex) {
   return knex.schema.createTable('items', table => {
     table.increments('id').primary();
-    table.string('image').notNullable();
+    table.string('image_url').notNullable();
     table.string('title').notNullable();
   });
 }
---
+++ server/src/database/seeds/create_items.ts
@@ -0,0 +1,12 @@
+import Knex from 'knex';
+
+export async function seed(knex: Knex) {
+  await knex('items').insert([
+    { title: 'Lâmpadas', image_url: 'lampadas.svg' },
+    { title: 'Pilhas e Baterias', image_url: 'baterias.svg' },
+    { title: 'Papéis e Papelão', image_url: 'papeis-papelao.svg' },
+    { title: 'Resíduos Eletrônicos', image_url: 'eletronicos.svg' },
+    { title: 'Resíduos Orgânicos', image_url: 'organicos.svg' },
+    { title: 'Óleo de cozinha', image_url: 'oleo.svg' },
+  ]);
+}
