3b0838827ed885c84c57f4bafbbb1d06e2b87735
Iury Vasconcelos <iury0393@gmail.com>
2020-06-04 13:40:42 -0300

    Conceitos

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+/*
+* JSX: Sintaxe de XML no JavaScript
+*/
+import Header from './Header';
+
 function App() {
+  const [counter, setCounter] = useState(0);//![valor do estado, função para atualizar o valor do estado]
+
+  function handleButtonClick() {
+    setCounter(counter + 1);
+  }
+
   return (
-    <h1>Hello World!</h1>
+    <div>
+      <Header title={`Contador: ${counter}`}/>
+
+      <h1>{counter}</h1>
+      <button type="button" onClick={handleButtonClick}>Aumentar</button>
+    </div>
   );
 }
 
